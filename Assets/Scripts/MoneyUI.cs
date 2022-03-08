e181cbace1c74b758896859249c9bf7ad1d8d482
Alexandre COULON <alexandre.coulon@sii.fr>
2022-03-08 14:57:04 +0100

    Smooth update money effect.

--- Assets/Scripts/MoneyUI.cs
+++ Assets/Scripts/MoneyUI.cs
@@ -4,9 +4,20 @@ using UnityEngine.UI;
 public class MoneyUI : MonoBehaviour
 {
     public Text moneyText;
+    public int smoothUpdateMultiplicator = 10;
+    private int amountDisplayed;
 
     void Update()
     {
-        moneyText.text = "$ " + PlayerStats.Money.ToString();
+        if (PlayerStats.Money < amountDisplayed)
+        {
+            amountDisplayed -= smoothUpdateMultiplicator;
+        }
+        else if (PlayerStats.Money > amountDisplayed)
+        {
+            amountDisplayed += smoothUpdateMultiplicator;
+        }
+        
+        moneyText.text = "$ " + amountDisplayed.ToString();
     }
 }
